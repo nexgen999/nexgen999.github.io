e5f2febc8ca8f0eaac931eec1f6a42a2a231955e
nexgen999 <nexgen999@gmail.com>
2026-05-28 23:26:22 +0200

    Add files via upload

--- X-free-Dashboard/sw.js
+++ X-free-Dashboard/sw.js
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-const CACHE_NAME = 'x-free-dashboard-cache-v3';
+const CACHE_NAME = 'x-free-dashboard-cache-v4';
 const ASSETS = [
   '/',
   '/index.html',
